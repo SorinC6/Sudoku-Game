4e6952a5c153dc8b65d2569f3dcaa35681c5a9ed
Sorin Chis <kish.sorin@yahoo.com>
2020-02-12 14:33:11 +0200

    added main styled components

---
+++ src/components/grid/styles/index.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react'
+
+const Grid: FC = () => {
+  return <div>TT</div>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom'
 
 import { unregister } from './core'
 import { GlobalStyles, theme } from './styles'
+import { Content, Title, Card } from './components'
 import { ThemeProvider } from 'styled-components'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <div>Hello</div>
+    <Content>
+      <Title>Sudoku</Title>
+      <Card>Work in Progress</Card>
+    </Content>
   </ThemeProvider>,
   document.getElementById('root')
 )
