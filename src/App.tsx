89c956a5b4b9e97c96a9f33e03ce33799f06b3b6
Sorin Chis <kish.sorin@yahoo.com>
2020-02-12 10:41:39 +0200

    setting up prettier and vscode settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-const App = () => {
+const App: FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -16,11 +16,11 @@ const App = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Sudoku
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
