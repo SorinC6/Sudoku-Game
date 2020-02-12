20763916dbd7991063d2bb1fe8305236257bc99a
Sorin Chis <kish.sorin@yahoo.com>
2020-02-12 11:26:05 +0200

    setup Theme Provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { unregister } from './core'
-import { GlobalStyles } from './styles'
+import { GlobalStyles, theme } from './styles'
+import { ThemeProvider } from 'styled-components'
 
 ReactDOM.render(
-  <>
+  <ThemeProvider theme={theme}>
     <GlobalStyles />
     <div>Hello</div>
-  </>,
+  </ThemeProvider>,
   document.getElementById('root')
 )
 
--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,24 +1,26 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 export default createGlobalStyle`
-  html{
-    height:100%;
+${({ theme }) => css`
+  html {
+    height: 100%;
 
-    body{
-      display:flex;
-      flex-direction:column;
-      height:100%;
-      margin:0;
+    body {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      margin: 0;
 
-      #root{
-        background:radial-gradient(#233c34cc,#281c34);
-        color:#281c34;
-        display:flex;
-        font-family:sans-serif;
-        height:100%;
-        justify-content:center;
-        padding:15px;
+      #root {
+        background: ${theme.colors.background};
+        color: ${theme.colors.black};
+        display: flex;
+        font-family: sans-serif;
+        height: 100%;
+        justify-content: center;
+        padding: 15px;
       }
     }
   }
+`}
 `
